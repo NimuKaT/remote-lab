ec2d9f7d5b14f3e1437da29cebd97b1612b33fe1
NimuKaT <NimuKaT@users.noreply.github.com>
2023-05-30 19:10:07 +1000

    Start work on server side

--- src/Server/app.tsx
+++ src/Server/app.tsx
@@ -1,7 +1,14 @@
-const express = require('express')
+import { env } from "node:process"
+import NIUSBDRiver from "./NIUSBDriver"
+import express from 'express'
+import readline from "readline"
+import fs from "fs/promises"
+
+// const express = require('express')
 // import express, { Express } from "express"
 let app = express()
 let port = 3000
+let niusb = new NIUSBDRiver('/')
 
 express.static("app/build")
 app.use(express.static('app/build'))
@@ -9,6 +16,43 @@ app.use(express.static('app/build'))
 //     res.sendFile
 // })
 
+app.get('/api/testWrite', (req, res) => {
+    niusb.writeToDevice({digital: [
+        [false, false, true, true, false,false,true, true],
+        [true,false,false,false,false,false,false,false]
+    ], analogue: [[false,false]]}, () => {
+
+    })
+    res.send("Hello")
+})
+
+app.get('/api/currentLab', (req, res, next) => {
+    res.json({
+        "labFile": labFile
+    })
+})
+
+// Select Lab file
+
+let specificationDir = "./bin/dist/Specifications/"
+let labFile = ""
+let rl = readline.createInterface({input: process.stdin, output: process.stdout})
+function getLabFile() {
+    rl.question("Please enter the file name of the lab file to be opened: ", (answer) => {
+        fs.readFile(specificationDir + answer, 'utf-8').then((data) => {
+            // read JSON
+            console.log(JSON.parse(data))
+            labFile = answer
+        }).catch((err) => {
+            // Handle error
+            console.log(err)
+            getLabFile()
+        })
+    })
+}
+
 app.listen(port, () => {
     console.log('Server listening to ' + port)
 })
+
+getLabFile()
